a9f93f3bb261e7b218a8329189e6461770f763b4
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-12 10:42:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+
+let isplaying=false;
+
+function playsound(sound){
+
+    let audio =   document.querySelector(`audio[src='${sound}']`);
+   
+    console.log(audio);
+    if(isplaying==false){
+        isplaying=true;
+        audio.play();
+    }
+    else{
+        isplaying=false;
+        audio.pause();
+    }
+    
+}
